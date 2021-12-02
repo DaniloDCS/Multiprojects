f07517071bc2ad9c1e6c5ff3e6743f2f5c039e87
Danilo <danillo.ifpb@gmail.com>
2021-12-02 11:42:18 -0300

    Anti-Ghosting

---
+++ public/js/anti-ghosting.js
@@ -0,0 +1,28 @@
+let keys = {};
+
+document.addEventListener("keydown", event => {
+    event.preventDefault();
+    let code = event.code;
+    keys[code] = true;
+    show();
+})
+
+document.addEventListener("keyup", event => {
+    event.preventDefault();
+    let code = event.code;
+    console.log(code)
+
+    keys[code] = false;
+    show();
+
+})
+
+function show() {
+    Object.keys(keys).forEach(key => {
+        if (keys[key]) {
+            document.querySelector(`#${key}`).classList.add("active");
+        } else {
+            document.querySelector(`#${key}`).classList.remove("active");
+        }
+    });
+}
